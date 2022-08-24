efee7b6158148ccd33183c2b5aa08d0ba605b568
PE-611 <PE-611@yandex.ru>
2022-08-24 19:03:44 +0300

    v1.0 Single read write with autorefresh, without burst mode

--- SDRAM_IS42S16160B_DRIVER.v
+++ SDRAM_IS42S16160B_DRIVER.v
@@ -4,23 +4,24 @@
 // Company : FLEXLAB													//
 // Description : Test system (shift_reg/driver)			  	//
 // Start design : 11.08.2022 										//
-// Last revision : 17.08.2022 									//
+// Last revision : 24.08.2022 									//
 ///////////////////////////////////////////////////////////
 
 
 
-module SDRAM_IS42S16160B_DRIVER( input SDRAM_CLK_IN,  start_write, start_read, reset,
-											input [11:0] ADDR_ROW,
-											input [11:0] ADDR_COL,
+module SDRAM_IS42S16160B_DRIVER( input SDRAM_CLK_IN, SDRAM_CLK_OUT,  start_write, start_read, reset,
+											input [12:0] ADDR_ROW,
+											input [12:0] ADDR_COL,
 											input [1:0]  BANK,
 
-											output reg	[11:0]	DRAM_ADDR,
+											output reg	[12:0]	DRAM_ADDR,
 											output reg	[1:0]		DRAM_BA,
 											output reg				DRAM_CKE,
 											output reg  [1:0]		DRAM_DQM,
 											output reg 	[3:0] 	DRAM_CMD,		//DRAM_CS_N, DRAM_RAS_N, DRAM_CAS_N, DRAM_WE_N
-											output reg				process_flg,	//PROCESS FLAG IS HERE!
-											inout  		[15:0]	DRAM_DQ
+											output reg				process_flg,
+											output reg				ready_data		//PROCESS FLAG IS HERE!
+											//inout  		[15:0]	DRAM_DQ
 											
 											//	output reg				DRAM_CAS_N,
 											//	output reg	    		DRAM_CS_N,
@@ -29,20 +30,21 @@ module SDRAM_IS42S16160B_DRIVER( input SDRAM_CLK_IN,  start_write, start_read, r
 										 );
 										 
 										 
-parameter Fclk								= 160_000_000;		  		  // MHz										 
+parameter Fclk								= 140_000_000;		  		  // MHz										 
 parameter tREF       					= 64;   						  // ms
-parameter tINIT							= 200;						  // us
+parameter tINIT							= 200_000;					  // ns
 parameter quantity_autorefresh 		= 8192;  					  // refresh counter (cycle refresh)
 
 parameter tCK        					= 7;     					  // clk period 7 ns (for calcul refresh interval)
-parameter REFRESH_TIME					= (((tREF * 1_000_000) / quantity_autorefresh) / tCK); // need 64 ms
+parameter REFRESH_TIME					= (((tREF * 1_000_000) / quantity_autorefresh) / tCK); 
+																				  // need 64 ms
 
-parameter init_time	 					= (Fclk / (22 * tINIT));	 					  // need time > 200 us
+parameter init_time	 					= tINIT / tCK;       		// need time > 200_000 ns
 parameter q_AREF_for_init				= 8;
-parameter MRS_DATA						= 12'b000_1_00_010_1_000; // mode register set
+parameter MRS_DATA						= 12'b000_1_00_011_1_000; // mode register set
 parameter trc								= 10;  						  // Trc
 parameter trp								= 3;							  // Trp 
-parameter cas			 					= 3;							  // CAS
+parameter cas			 					= 6;							  // CAS
 parameter rcd								= 3;							  // Rcd
 
 localparam IDLE 							= 8'd0;
@@ -54,7 +56,8 @@ localparam WRITE							= 8'd5;
 localparam PRECHARGE						= 8'd6;
 localparam NOP_INIT_AREF			  	= 8'd7;
 localparam NOP_BEFORE_RW				= 8'd8;
-localparam NOP_AFTER_RW					= 8'd9;
+localparam NOP_AFTER_W					= 8'd9;
+localparam NOP_AFTER_R					= 8'd10;
 										 						 
 localparam CMD_DESL  					= 4'b1_1_1_1; // device deselect
 localparam CMD_NOP   					= 4'b0_1_1_1; // no operation
@@ -95,7 +98,7 @@ initial begin
 	DRAM_CKE 						<= 1'b1;
 	DRAM_CMD							<= CMD_NOP;
 	DRAM_ADDR 						<= 12'd0;
-	DRAM_BA 							<= 1'b0;
+	DRAM_BA 							<= 1'b00;
 	DRAM_DQM 						<= 1'b0;
 	
 	cnt_timer	 					<= 24'd0;
@@ -129,7 +132,7 @@ always @*
 					next_state <= PRECHARGE;
 				end
 								
-				else if (write_flg == 1'b1 && process_flg == 1'b0 || read_flg == 1'b1 && process_flg == 1'b0) begin
+				else if (write_flg == 1'b1 || read_flg == 1'b1) begin  
 					next_state <= ACTIVATE_ROW;
 				end
 								
@@ -232,7 +235,7 @@ always @*
 						
 				
 				if (state == READ) begin
-					next_state <= NOP_AFTER_RW;
+					next_state <= NOP_AFTER_R;
 				end
 				
 							
@@ -245,7 +248,7 @@ always @*
 						
 				
 				if (state == WRITE) begin
-					next_state <= NOP_AFTER_RW;
+					next_state <= NOP_AFTER_W;
 				end
 				
 							
@@ -254,14 +257,24 @@ always @*
 				end
 				
 				
-			NOP_AFTER_RW:
+			NOP_AFTER_R:
 				
-				if (Tcas == cas) begin
+				if (Tcas == cas) begin						// CAS = 3 and 3 for Trc for next ACTIVATE comand
 					next_state <= IDLE;
 				end		
 				
 				else begin
-					next_state <= NOP_AFTER_RW;
+					next_state <= NOP_AFTER_R;
+				end
+				
+			NOP_AFTER_W:
+				
+				if (Tcas == cas) begin						// CAS = 3 and 3 for Trc for next ACTIVATE comand
+					next_state <= IDLE;
+				end		
+				
+				else begin
+					next_state <= NOP_AFTER_W;
 				end
 				
 			
@@ -275,35 +288,21 @@ always @(posedge SDRAM_CLK_IN) begin
 	
 	cnt_timer <= cnt_timer + 1'b1;
 	DRAM_CKE <= 1'b1;
+	DRAM_DQM	<= 1'b0;
 	
 	
-	
-	
-	
-	if (start_write == 1'b0) begin
-		write_flg <= 1'b1;
-	end
-	
-	if (start_read == 1'b0) begin
-		read_flg <= 1'b1;
-	end
-	
-	
-	
-	
-	
-	
-	
+		
 	if (state == IDLE) begin
 		DRAM_CMD 						<= CMD_NOP;
 		DRAM_DQM 						<= 1'b0;
 		process_flg 					<= 1'b0;
+		
 	end
 	
 	if (state == PRECHARGE) begin
 		process_flg 					<= 1'b1;
 		DRAM_CMD  						<= CMD_PRE;
-		DRAM_ADDR 						<= 12'b001000000000;				// ALL BANK
+		DRAM_ADDR 						<= 12'b010000000000;				// ALL BANK
 		DRAM_BA							<= BANK;
 	end
 		
@@ -328,7 +327,7 @@ always @(posedge SDRAM_CLK_IN) begin
 	if (state == MRS) begin
 		DRAM_CMD  						<= CMD_MRS;
 		DRAM_ADDR 						<= MRS_DATA;
-		cnt_autorefresh 				<= 16'd0;
+		cnt_autorefresh 				<= 8'd0;
 		DRAM_BA							<= BANK;
 	end
 	
@@ -354,30 +353,47 @@ always @(posedge SDRAM_CLK_IN) begin
 	
 	if (state == WRITE) begin
 		DRAM_CMD 						<= CMD_WRITE;
-		DRAM_ADDR						<= ADDR_COL;
-		DRAM_BA							<= BANK;
-		write_flg						<= 1'b0;
+		DRAM_ADDR						<= ADDR_COL + 1024;  //1024 for set a10 bit in high level. For enable autoprecharge ALL banks
+		DRAM_BA							<= BANK;				  	 
 	end
 	
 	if (state == READ) begin
 		DRAM_CMD 						<= CMD_READ;
-		DRAM_ADDR						<= ADDR_COL;
+		DRAM_ADDR						<= ADDR_COL + 1024;  //1024 for set a10 bit in high level. For enable autoprecharge ALL banks
 		DRAM_BA							<= BANK;
-		read_flg							<= 1'b0;
+//		ready_data						<= 1'b0;
 	end
 	
-	if (state == NOP_AFTER_RW) begin
+	if (state == NOP_AFTER_R) begin
+		DRAM_CMD 						<= CMD_NOP;
 		Tcas 								<= Tcas + 1'b1;
+		read_flg <= 1'b0;
+	end
+
+	
+	if (state == NOP_AFTER_W) begin
+		DRAM_CMD 						<= CMD_NOP;
+		Tcas 								<= Tcas + 1'b1;
+		write_flg <= 1'b0;
 	end
 	
-	if (state != NOP_AFTER_RW) begin
-		Tcas		 						<= 1'b0;
-	end
 	
 	if (Tcas == cas) begin
 		Tcas 								<= 4'd0;
 	end
 	
+	
+	
+	
+	if (start_write == 1'b0) begin
+		write_flg 						<= 1'b1;
+		process_flg 					<= 1'b1;
+	end
+	
+	if (start_read == 1'b0) begin
+		read_flg 						<= 1'b1;
+		process_flg 					<= 1'b1;
+	end
 	
 	
 
@@ -400,16 +416,31 @@ always @(posedge SDRAM_CLK_IN) begin
 		flg_init <= 1'b1;
 	end
 	
-	if (cnt_timer < init_time + 2 && flg_init == 1'b0) begin		// +1 takt chtob process_flg ne provalivalsa poka flg_init ne stanet 1
+	if (cnt_timer < init_time + 2 && flg_init == 1'b0) begin			// +1 takt chtob process_flg ne provalivalsa poka flg_init ne stanet 1
 		process_flg <= 1'b1;
 	end
 	
 	if (Trc == trc && flg_init == 1'b1)  begin   
 		self_refresh <= 1'b0;
 	end
+	
+//	if (state == NOP_AFTER_R && Tcas == 4'd3) begin
+//		ready_data   <= 1'b1;
+//	end	
 
 	
-end	
+end
+
+
+always @(posedge SDRAM_CLK_OUT) begin
+	if (state == NOP_AFTER_R && Tcas == 4'd3) begin
+		ready_data   <= 1'b1;
+	end
+	
+	if (state == READ) begin
+		ready_data	<= 1'b0;
+	end
+end 	
 
 
 always @(posedge SDRAM_CLK_IN or negedge reset) begin 
